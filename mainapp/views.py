03290209d68a9659ad3ff1dc936ac02a7873ac00
FranciscoRamos01 <fr0030160@gmail.com>
2025-11-29 14:44:51 -0500

    Delete mainapp directory

--- mainapp/views.py
+++
@@ -1,9 +0,0 @@
-
-from django.http import HttpResponse
-from django.shortcuts import render
-
-def pruebas(request):
-    return HttpResponse("<h1>Bienvenido a Django desde Unitecnar</h1>")
-
-def template_view(request):
-    return render(request, 'index.html')
